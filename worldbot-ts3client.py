1bf0ed9726a9a2507cfdc10a69165a2f9898303e
Isaac Khor <s.github@isaackhor.com>
2021-01-05 21:54:22 -0500

    Modify hotswap to also hotswap the ts3shim

--- worldbot-ts3client.py
+++ worldbot-ts3client.py
@@ -18,13 +18,16 @@ def pull_and_reload():
     # Pull from github
     os.system('git pull')
     global worldbot
+    global ts3shim
     worldbot = importlib.reload(worldbot)
+    ts3shim = importlib.reload(ts3shim)
 
 conn = ts3shim.ClientqueryConn(host=HOST, port=PORT, apikey=CLIENTQUERY_API_KEY)
 botcore = worldbot.WorldBot()
 
 def on_notify_msg(msg):
     global botcore
+    global conn
     if msg.invokername == NICKNAME:
         return
 
@@ -33,6 +36,7 @@ def on_notify_msg(msg):
         msg.msg.lower().strip() == '.redeploy secretredeploypassword'):
         pull_and_reload()
         botcore = worldbot.WorldBot()
+        conn = ts3shim.ClientqueryConn(host=HOST, port=POST, apikey=CLIENTQUERY_API_KEY)
         return 'Redeploy successful'
 
     if (msg.msg.startswith('.exit') and
